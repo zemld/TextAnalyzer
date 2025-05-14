959294564e42e614636b843c489074440ff27509
zemld <zemlyanukhind@yandex.ru>
2025-05-14 21:41:33 +0300

    Объявлены хэндлеры для анализа файла

---
+++ file-analyzer/handlers/handlers.go
@@ -0,0 +1,13 @@
+package handlers
+
+import (
+	"net/http"
+)
+
+func AnalyzeFileHandler(w http.ResponseWriter, r *http.Request) {
+	// TODO: получаем на вход айди файла. Проводим его анализ.
+}
+
+func WordCloudHandler(w http.ResponseWriter, r *http.Request) {
+	// TODO: получаем на вход айди файла. Проводим облако слов.
+}

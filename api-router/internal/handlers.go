d7eb2f2a42c5eafda2270ce3531746f008f278ea
zemld <zemlyanukhind@yandex.ru>
2025-05-14 18:22:59 +0300

    Объявлены методы обработки запросов с апи

---
+++ api-router/internal/handlers.go
@@ -0,0 +1,17 @@
+package internal
+
+import (
+	"net/http"
+)
+
+func UploadFileHandler(w http.ResponseWriter, r *http.Request) {
+}
+
+func DownloadFileHandler(w http.ResponseWriter, r *http.Request) {
+}
+
+func AnalyzeFileHandler(w http.ResponseWriter, r *http.Request) {
+}
+
+func WordCloudHandler(w http.ResponseWriter, r *http.Request) {
+}

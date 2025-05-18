7e45b9df18003d1eed15495c5b0423b95472b87c
zemld <zemlyanukhind@yandex.ru>
2025-05-18 12:52:23 +0300

    Изменено описание параметров id

--- file-storager/handlers/handlers.go
+++ file-storager/handlers/handlers.go
@@ -6,7 +6,7 @@ import (
 
 // @description Check if file exists in DB.
 // @tag.name File operations
-// @param id path int true "Id of file"
+// @param id path int true "File ID"
 // @produce json
 // @success 200 {object} FileExistsResponse
 // @failure 401 {object} FileExistsResponse
@@ -18,7 +18,7 @@ func CheckFileExistsHandler(w http.ResponseWriter, r *http.Request) {
 // @description Upload file to DB.
 // @tag.name File operations
 // @param file formData file true "File to upload"
-// @param id formData int true "Id of file"
+// @param id formData int true "File ID"
 // @produce json
 // @success 200 {object} FileStatusResponse
 // @failure 500 {object} FileStatusResponse
@@ -29,9 +29,9 @@ func UploadFileHandler(w http.ResponseWriter, r *http.Request) {
 
 // @description Download file from DB.
 // @tag.name File operations
-// @param id path int true "Id of file"
+// @param id path int true "File ID"
 // @produce plain
-// @success 200 {formData} file
+// @success 200 {file} blob
 // @success 401 {object} FileStatusResponse
 // @failure 500 {object} FileStatusResponse
 // @router /files/{id} [get]
@@ -41,7 +41,7 @@ func GetFileHandler(w http.ResponseWriter, r *http.Request) {
 // @description Save analysis result to DB.
 // @tag.name File operations
 // @accept json
-// @param id path int true "Id of file"
+// @param id path int true "File ID"
 // @produce json
 // @success 200 {object} FileStatusResponse
 // @failure 500 {object} FileStatusResponse
@@ -52,7 +52,7 @@ func SaveAnalysisResultHandler(w http.ResponseWriter, r *http.Request) {
 
 // @description Get analysis result from DB. Result contains amount of paragraphs, sentences, words, symbols. Also contains average amount of sentences per paragraph, words per sentence, length of words.
 // @tag.name File operations
-// @param id path int true "Id of file"
+// @param id path int true "File ID"
 // @produce json
 // @success 200 {object} AnalysisResponse
 // @failure 401 {object} FileStatusResponse
@@ -64,7 +64,7 @@ func GetAnalysisResultHandler(w http.ResponseWriter, r *http.Request) {
 // @description Save word cloud to DB.
 // @tag.name File operations
 // @accept png
-// @param id path int true "Id of file"
+// @param id path int true "File ID"
 // @param wordCloud formData file true "Word cloud to save"
 // @produce json
 // @success 200 {object} FileStatusResponse
@@ -76,7 +76,7 @@ func SaveWordCloudHandler(w http.ResponseWriter, r *http.Request) {
 
 // @description Get word cloud from DB.
 // @tag.name File operations
-// @param id path int true "Id of file"
+// @param id path int true "File ID"
 // @produce png
 // @success 200 {file} blob
 // @failure 401 {object} FileStatusResponse

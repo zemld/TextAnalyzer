721099a23e0426bad4f18d3075938076ae8e5d0b
zemld <zemlyanukhind@yandex.ru>
2025-05-20 19:57:17 +0300

    Изменена тип аргумента для функций написания ответов

--- file-storager/handlers/internal.go
+++ file-storager/handlers/internal.go
@@ -5,24 +5,26 @@ import (
 	"net/http"
 )
 
-func writeFileExistsResponse(w *http.ResponseWriter, resp FileExistsResponse) {
-	(*w).Header().Add("Content-Type", "application/json")
+// TODO: имеет смысл добавить кастомный тип для id.
+
+func writeFileExistsResponse(w http.ResponseWriter, resp FileExistsResponse) {
+	w.Header().Add("Content-Type", "application/json")
 	repsJson, _ := json.Marshal(resp)
-	(*w).Write(repsJson)
+	w.Write(repsJson)
 }
 
-func writeBadFileExistsResponse(w *http.ResponseWriter) {
+func writeBadFileExistsResponse(w http.ResponseWriter) {
 	resp := FileExistsResponse{Exists: false, Id: -1, Status: "File doesn't exist"}
 	writeFileExistsResponse(w, resp)
 }
 
-func writeGoodFileExistsResponse(w *http.ResponseWriter, id int) {
+func writeGoodFileExistsResponse(w http.ResponseWriter, id int) {
 	resp := FileExistsResponse{Exists: true, Id: id, Status: "File exists"}
 	writeFileExistsResponse(w, resp)
 }
 
-func writeFileStatusResponse(w *http.ResponseWriter, id int, msg string) {
-	(*w).Header().Add("Content-Type", "application/json")
+func writeFileStatusResponse(w http.ResponseWriter, id int, msg string) {
+	w.Header().Add("Content-Type", "application/json")
 	repsJson, _ := json.Marshal(FileStatusResponse{id, msg})
-	(*w).Write(repsJson)
+	w.Write(repsJson)
 }

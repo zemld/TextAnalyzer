ccb1a34ecfb98387c164852c3315a1dfbfd0d4b2
zemld <zemlyanukhind@yandex.ru>
2025-05-23 18:56:30 +0300

    Написана swagger документация

--- file-analyzer/handlers/handlers.go
+++ file-analyzer/handlers/handlers.go
@@ -4,12 +4,25 @@ import (
 	"net/http"
 )
 
+// @description Analyze file.
+// @tag.name File operations
+// @param id path int true "File ID"
+// @produce json
+// @success 200 {object} Analysis
+// @failure 400 {object} FileStatusResponse
+// @failure 500 {object} FileStatusResponse
+// @router /files/analyze/{id} [get]
 func AnalyzeFileHandler(w http.ResponseWriter, r *http.Request) {
-	// TODO: получаем на вход айди файла. Проводим его анализ.
-	// Отдаем результат анализа на выход.
+
 }
 
+// @description Get word cloud.
+// @tag.name File operations
+// @param id path int true "File ID"
+// @produce png
+// @success 200 {object} blob
+// @failure 400 {object} FileStatusResponse
+// @failure 500 {object} FileStatusResponse
 func WordCloudHandler(w http.ResponseWriter, r *http.Request) {
-	// TODO: получаем на вход айди файла. Проводим облако слов.
-	// Отдаем картинку с облаком на выход.
+
 }

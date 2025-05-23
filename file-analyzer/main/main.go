01b0eee6b3ab1ef9e363040e29438434953aa108
zemld <zemlyanukhind@yandex.ru>
2025-05-24 01:45:54 +0300

    Добавлена swagger документация

--- file-analyzer/main/main.go
+++ file-analyzer/main/main.go
@@ -4,14 +4,23 @@ import (
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
+	httpSwagger "github.com/swaggo/http-swagger"
 	"github.com/zemld/TextAnalyzer/file-analyzer/handlers"
 )
 
+// @title File Analyzer
+// @version 1.0
+// @description Service for analyzing files.
+// @host localhost:8083
+// @BasePath /
 func main() {
 	router := chi.NewRouter()
-	// TODO: здесь надо передавать сам файл, который мы будем анализировать.
-	router.Get("/files/analyze/{id}", handlers.AnalyzeFileHandler)
-	router.Get("/files/wordcloud/{id}", handlers.WordCloudHandler)
+	router.Get("/files/analyze", handlers.AnalyzeFileHandler)
+	router.Get("/files/wordcloud", handlers.WordCloudHandler)
 
-	http.ListenAndServe(":8080", nil)
+	fs := http.FileServer(http.Dir("./docs"))
+	router.Handle("/docs/*", http.StripPrefix("/docs/", fs))
+	router.Get("/swagger/*", httpSwagger.Handler(httpSwagger.URL("http://localhost:8083/docs/swagger.json")))
+
+	http.ListenAndServe(":8083", nil)
 }

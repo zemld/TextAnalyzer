02c3b66481077343099db282e52836bfb3639efd
zemld <zemlyanukhind@yandex.ru>
2025-05-25 09:24:52 +0300

    Созданы модели для ответов

---
+++ api-router/handlers/responses.go
@@ -0,0 +1,17 @@
+package handlers
+
+type FileStatusResponse struct {
+	Id     int    `json:"id"`
+	Status string `json:"status"`
+}
+
+type Analysis struct {
+	Id                           int     `json:"id"`
+	ParagraphsAmount             int     `json:"paragraphs_amount"`
+	SentencesAmount              int     `json:"sentences_amount"`
+	WordsAmount                  int     `json:"words_amount"`
+	SymbolsAmount                int     `json:"symbols_amount"`
+	AverageSentencesPerParagraph float64 `json:"average_sentences_per_paragraph"`
+	AverageWordsPerSentence      float64 `json:"average_words_per_sentence"`
+	AverageLengthOfWords         float64 `json:"average_length_of_words"`
+}
